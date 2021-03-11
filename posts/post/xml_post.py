a69581d54eb6d414d638547bedc7fb3b90c81ca8
BIRD-UP73 <birdupthemusical@outlook.com>
2021-03-11 19:09:05 +0100

    Handle case with no posts after fetching

--- posts/post/xml_post.py
+++ posts/post/xml_post.py
@@ -5,7 +5,7 @@ from xml.etree import ElementTree
 from discord.ext.commands import Context, CommandError
 
 from posts.api.xml_api import send_request
-from posts.data.post_data import PostData
+from posts.data.post_data import PostData, PostNoLongerExists
 from posts.data.xml_post_data import XmlPostData
 from posts.post.post_message import PostMessage
 from util import util
@@ -65,6 +65,6 @@ def fetch_xml_post(url: str, tags: str, page: int) -> Element:
     posts = ElementTree.fromstring(resp_text)
 
     if len(posts) == 0:
-        return
+        return PostNoLongerExists()
 
     return XmlPostData.from_xml(posts[0])
