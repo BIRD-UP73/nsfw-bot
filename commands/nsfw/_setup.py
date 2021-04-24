72bb7235e3787a79f6b4eed6d1431e8423a3b8a5
BIRD-UP73 <birdupthemusical@outlook.com>
2021-04-24 21:19:05 +0200

    Added support for Konachan

--- commands/nsfw/_setup.py
+++ commands/nsfw/_setup.py
@@ -3,6 +3,7 @@ from discord.ext.commands import Bot
 from commands.nsfw.danbooru import DanbooruCommand
 from commands.nsfw.gelbooru import GelbooruCommand
 from commands.nsfw.hypnohub import HypnohubCommand
+from commands.nsfw.konachan_com import KonachanComCommand
 from commands.nsfw.rule34_paheal import Rule34PahealCommand
 from commands.nsfw.rule_34 import Rule34Command
 from commands.nsfw.tbib import TbibCommand
@@ -13,6 +14,7 @@ def setup(bot: Bot):
     bot.add_command(DanbooruCommand())
     bot.add_command(GelbooruCommand())
     bot.add_command(HypnohubCommand())
+    bot.add_command(KonachanComCommand())
     bot.add_command(Rule34Command())
     bot.add_command(Rule34PahealCommand())
     bot.add_command(TbibCommand())
---
+++ commands/nsfw/konachan_com.py
@@ -0,0 +1,7 @@
+from commands.nsfw.nsfw_command import NsfwCommand
+from url.urls import KonachanCom
+
+
+class KonachanComCommand(NsfwCommand):
+    name = 'konachan.com'
+    url = KonachanCom
